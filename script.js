1e57289e2353240a2081d28e5ea53d0f7dfd2903
PatrickStojek <patryk.stojek7@gmail.com>
2024-08-27 18:50:19 +0200

    add example of event bubbling

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ function incrementCounter() {
 }
 
 btn.addEventListener('click', incrementCounter)
+
+///////////////////////////////////////////////////////////////////
+const section1 = document.querySelector('.section')
+
+const addClassName = function() {
+    const className = this.tagName.toLowerCase();
+    this.classList.add(className)
+    console.log(className + " was clicked")
+}
+
+if(btn && section1) {
+    btn.addEventListener('click', addClassName)
+    section1.addEventListener('click', addClassName)
+}
+
+//showcase bubbling phase
+const itemList = document.querySelectorAll('.bubblingSection, bubblingArticle, h1')
+
+const showTagName = function() {
+    console.log(this.tagName)
+}
+
+itemList.forEach((item) => {
+    item.addEventListener('click', showTagName, true)
+})
